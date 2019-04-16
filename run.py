b815128067ab04424956a8ecfb0d7e5a4d21ea88
Lewis <rackateen@gmail.com>
2019-04-16 19:51:25 +0300

    My 8th commit

--- run.py
+++ run.py
@@ -1,11 +1,15 @@
-from flask import Flask
+from flask import Flask, jsonify, request
 
 my_app = Flask(__name__)
 
 
-@my_app.route('/')
-def hello_world():
-    return 'Hello World!'
+@my_app.route('/', methods=['GET', 'POST'])
+def index():
+	if (request.method == 'POST'):
+		some_json = request.get_json()
+		return jsonify({'you sent': some_json}), 201
+	else:
+    		return jsonify({"about":"Hello World!"})
 
 
 if __name__ == '__main__':
